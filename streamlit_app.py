f8d3a31d0e8adc43b9bd6050497fa76e2d0b394a
jyyy57 <kimyao557@gmail.com>
2023-06-19 11:15:01 -0500

    first commit

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,70 @@
 import streamlit as st
+from streamlit_chat import message
+from streamlit_extras.colored_header import colored_header
+from streamlit_extras.add_vertical_space import add_vertical_space
+from hugchat import hugchat
+from hugchat.login import Login
 
-st.title('🎈 App Name')
+st.title('🎈 My LLM-powered chatbot')
 
 st.write('Hello world!')
+
+with st.sidebar:
+    st.title('🤗💬 HugChat App')
+    st.markdown('''
+    ## About
+    This app is an LLM-powered chatbot built using:
+    - [Streamlit](<https://streamlit.io/>)
+    - [HugChat](<https://github.com/Soulter/hugging-chat-api>)
+    - [OpenAssistant/oasst-sft-6-llama-30b-xor](<https://huggingface.co/OpenAssistant/oasst-sft-6-llama-30b-xor>) LLM model
+
+    💡 Note: No API key required!
+    ''')
+    add_vertical_space(5)
+
+# login
+sign = Login(st.secrets["email"], st.secrets["password"])
+cookies = sign.login()
+sign.saveCookies()
+
+
+if 'generated' not in st.session_state:
+    st.session_state['generated'] = ["I'm HugChat, How may I help you?"]
+
+if 'past' not in st.session_state:
+    st.session_state['past'] = ['Hi!']
+
+    input_container = st.container()
+    colored_header(label='', description='', color_name='blue-30')
+    response_container = st.container()
+
+# User input
+## Function for taking user provided prompt as input
+def get_text():
+    input_text = st.text_input("You: ", "", key="input")
+    return input_text
+
+## Applying the user input box
+with input_container:
+    user_input = get_text()
+
+
+# Response output
+## Function for taking user prompt as input followed by producing AI generated responses
+def generate_response(prompt):
+    chatbot = hugchat.ChatBot(cookies=cookies.get_dict())
+    response = chatbot.chat(prompt)
+    return response
+
+
+## Conditional display of AI generated responses as a function of user provided prompts
+with response_container:
+    if user_input:
+        response = generate_response(user_input)
+        st.session_state.past.append(user_input)
+        st.session_state.generated.append(response)
+
+if st.session_state['generated']:
+    for i in range(len(st.session_state['generated'])):
+        message(st.session_state['past'][i], is_user=True, key=str(i) + '_user')
+        message(st.session_state["generated"][i], key=str(i))
